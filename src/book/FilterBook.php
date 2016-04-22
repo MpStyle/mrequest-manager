1edc95887f9a53631c7ec1d75124a02f1398376a
mtoolkit <mtoolkit.framework@gmail.com>
2016-04-22 17:13:30 +0200

    Added the license in the header of all files.

--- src/book/FilterBook.php
+++ src/book/FilterBook.php
@@ -1,4 +1,23 @@
 <?php
+/*
+ * This file is part of mrequest-manager.
+ *
+ * mrequest-manager is free software: you can redistribute it and/or modify
+ * it under the terms of the LGNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mrequest-manager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LGNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the LGNU Lesser General Public License
+ * along with mrequest-manager.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author  Michele Pagnin
+ */
+
 namespace mpstyle\mrequestmanager\book;
 
 use mpstyle\mrequestmanager\entity\Filter;
@@ -8,7 +27,7 @@ class FilterBook
 {
     /**
      * @param array $filtersArray
-     * @return \mpstyle\request\entity\Filter[]
+     * @return \mpstyle\mrequestmanager\entity\Filter[]
      */
     public function transform( array $filtersArray )
     {
